853244805b558c666755747d8f1d75669e537893
Kwagel <10807743-Kwagel@users.noreply.gitlab.com>
2022-05-24 10:41:42 +0100

    fizzBuzz using do while loop

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,3 @@
-import java.util.stream.IntStream;
-
 public class FizzBuzz {
     public static void main(String[] args) {
         fizzBuzz(10000);
@@ -7,8 +5,19 @@ public class FizzBuzz {
     }
     
     public static void fizzBuzz(int num) {
-        IntStream.rangeClosed(1, num).mapToObj(i -> i % 5 == 0 ? (i % 7 == 0 ? "FizzBuzz" : "Fizz") : (
-                i % 7 == 0 ? "Buzz" : i)).forEach(System.out::println);
+        int i = 0;
+        do {
+            i++;
+            if (((i % 5) == 0) && ((i % 7) == 0)) // Is it a multiple of 5 & 7?
+                System.out.println("fizzbuzz");
+            else if ((i % 5) == 0) // Is it a multiple of 5?
+                System.out.println("fizz");
+            else if ((i % 7) == 0) // Is it a multiple of 7?
+                System.out.println("buzz");
+            else
+                System.out.println(i); // Not a multiple of 5 or 7
+        } while (i < num);
+        
         
     }
     
